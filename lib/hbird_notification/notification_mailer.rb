46935a48ebd9a7e022355e84abfe0a1a0cff2852
Charlie Greene <greeneca@gmail.com>
2014-09-02 15:15:06 -0300

    fix from address

--- lib/hbird_notification/notification_mailer.rb
+++ lib/hbird_notification/notification_mailer.rb
@@ -1,9 +1,8 @@
 module HbirdNotification
   class NotificationMailer < ActionMailer::Base
-    default from: Config.hash[:mail_sender]
 
     def notify_user(address, subject, body)
-      mail(to: address, subject: subject) do |format|
+      mail(from: Config.hash[:mail_sender], to: address, subject: subject) do |format|
         format.text { render text: body }
       end
     end
